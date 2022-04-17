37a459744253e56de73394d0f0f8c6b2b8e0e4fe
Azian <azianas012@gmail.com>
2022-04-17 22:13:25 +0800

    Init - Ignore .env file, naikin templete .env.example - Edit config.js, ambil initial value dari .env variables - Fixed curriculum route di server/index.js - Add functionality to findOne and update curriculum

--- server/routes/CurriculumRoute.js
+++ server/routes/CurriculumRoute.js
@@ -1,11 +1,22 @@
-import { Router } from "express";
-import IndexController from "../controller/IndexController";
-import uploadDownload from "../middleware/uploadDownload"
-const router = Router()
+import { Router } from 'express';
+import IndexController from '../controller/IndexController';
+import uploadDownload from '../middleware/uploadDownload';
+import UploadDownloadHelper from '../helpers/UploadDownloadHelper';
 
-router.get('/',IndexController.CurriculumController.findAll)
-router.get("/images/:filename",uploadDownload.show_curr_logo)
-router.post('/',uploadDownload.uploadFiles,IndexController.CurriculumController.createCurr)
+const router = Router();
 
+router.get('/', IndexController.CurriculumController.findAll);
+router.post(
+  '/',
+  uploadDownload.uploadFiles,
+  IndexController.CurriculumController.createCurr
+);
+router.get('/:id', IndexController.CurriculumController.findOne);
+router.put(
+  '/:id',
+  UploadDownloadHelper.uploadSingleFile,
+  IndexController.CurriculumController.update
+);
+router.get('/images/:filename', uploadDownload.show_curr_logo);
 
-export default router
+export default router;
