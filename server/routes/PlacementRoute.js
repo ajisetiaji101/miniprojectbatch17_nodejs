fc948f11e135b36f603c5215ece5b9bd23c3839b
fadhlur497 <fadhlurrahman497@gmail.com>
2022-04-17 15:11:00 +0700

    APP_Talent

--- server/routes/PlacementRoute.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import IndexController from "../controller/IndexController";
-
-const router = Router();
-
-router.get("/", IndexController.PlacementController.List);
-
-export default router;
--- server/routes/TalentRoute.js
+++ server/routes/TalentRoute.js
@@ -3,7 +3,7 @@ import IndexController from "../controller/IndexController";
 
 const router = Router();
 
-router.get("/detail", IndexController.TalentController.detail);
+
 router.get("/", IndexController.TalentController.findAll);
 
 export default router;
