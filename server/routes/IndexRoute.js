e9fa48ad5d6b1fdbc197ef689d465913065eaba4
fif07 <muhammadafifsaputra07@gmail.com>
2022-04-17 23:42:22 +0700

    fix config

--- server/routes/IndexRoute.js
+++ server/routes/IndexRoute.js
@@ -2,12 +2,22 @@ import UserRoute from "./UserRoute";
 import PlacementRoute from "./PlacementRoute";
 import BatchRoute from './BatchRoute';
 import TalentRoute from './TalentRoute';
-
-
+import InstructorRoute from "./InstructorRoute";
+import CurriculumRoute from "./CurriculumRoute";
+import CurriculumMateriRoute from "./CurriculumMateriRoute";
+import CurriculumReviewsRoute from "./CurriculumReviewsRoute";
+import JobRoute from './JobRoute';
 
 
 export default {
   UserRoute,
+  PlacementRoute,
+  BatchRoute,
   TalentRoute,
+  InstructorRoute,
+  CurriculumRoute,
+  CurriculumMateriRoute,
+  CurriculumReviewsRoute,
+  JobRoute
 };
 
