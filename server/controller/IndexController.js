d626bb79339c129ad17c3d2d7e264854808b638e
fadhlur497 <fadhlurrahman497@gmail.com>
2022-04-17 23:56:37 +0700

    test

--- server/controller/IndexController.js
+++ server/controller/IndexController.js
@@ -2,9 +2,11 @@ import UserController from "./UserController";
 import PlacementController from "./PlacementController";
 import BatchController from "./BatchController";
 import TalentController from "./TalentController";
-
-
-
+import InstructorController from "./instructorController";
+import CurriculumController from "./CurriculumController";
+import CurriculumMateriController from "./CurriculumMateriController";
+import CurriculumReviewsController from "./CurriculumReviewsController";
+import TalentController from "./TalentController";
 
 export default {
   UserController,
