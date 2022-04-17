634e61264f5923ba0f5633dbf9c8cd7c0dcff0ec
Iqsan01 <iksaniqsan01@gmail.com>
2022-04-17 16:52:44 +0800

    update create curriculum

--- server/routes/CurriculumRoute.js
+++ server/routes/CurriculumRoute.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import IndexController from "../controller/IndexController";
-
+import uploadDownload from "../middleware/uploadDownload"
 const router = Router()
 
 router.get('/',IndexController.CurriculumController.findAll)
+router.get("/images/:filename",uploadDownload.show_curr_logo)
+router.post('/',uploadDownload.uploadFiles,IndexController.CurriculumController.createCurr)
 
 
 export default router
