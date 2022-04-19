91d6847d22b166795d97bd2f7cbfdd4bd9b88872
dwiekyims <dwiekyiskandar31@gmail.com>
2022-04-19 21:00:30 +0700

    findAll

--- server/controller/BatchController.js
+++ server/controller/BatchController.js
@@ -1,11 +1,24 @@
 const findAll = async(req,res)=>{
   try {
-    const batch = await req.context.models.batch.findAll({
-      include : [{
-        all : true
-      }]
+    const batch = await req.context.models.talent_batch.findAll({
+      include : [
+        {
+          model : req.context.models.batch,
+          as: "taba_batch",
+          include : [
+            {
+              model : req.context.models.instructor,
+              as : "batch_inst"
+            },
+          ],
+        },
+        {
+          model : req.context.models.talent,
+          as : "taba_tale"
+        },
+    ]
     })
-    return res.status(201).json({batch})
+    return res.status(201).json({ batch })
   } catch (error){
     return res.status(404).send("not found")
   }
