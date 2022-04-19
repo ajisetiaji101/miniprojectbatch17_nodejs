dbf636197b1c0588b3478ca89736269ffeecd200
Iqsan01 <iksaniqsan01@gmail.com>
2022-04-19 14:24:43 +0800

    upadate

--- server/controller/CurriculumController.js
+++ server/controller/CurriculumController.js
@@ -1,31 +1,6 @@
 const findAll = async (req,res) =>{
     try {
-        const result = await req.context.models.curriculum.findAll({
-            include: [
-                {
-                    model: await req.context.models.instructor,
-                    as: "curr_inst",
-                    include: [
-                        {
-                            model: await req.context.models.batch,
-                            as: "batches",
-                            include: [
-                                {
-                                    model: await req.context.models.talent_batch,
-                                    as: "talent_batches",
-                                    include: [
-                                        {
-                                            model: await req.context.models.talent,
-                                            as: "taba_tale"
-                                        }
-                                    ]
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]
-        })
+        const result = await req.context.models.curriculum.findAll()
         return res.json(200).send(result)
     } catch (error) {
         return res.status(404).send('no data found')
