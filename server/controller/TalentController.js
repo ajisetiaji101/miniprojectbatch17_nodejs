c3bc87bde7eb928e65f1bb69f4f6ce35aa521f24
fadhlur497 <fadhlurrahman497@gmail.com>
2022-04-20 01:39:05 +0700

    update findall

--- server/controller/TalentController.js
+++ server/controller/TalentController.js
@@ -1,11 +1,95 @@
-const findAll = async (req, res) => {
+// const findAll = async (req,res) =>{
+//   try {
+//       const result = await req.context.models.talent.findAll({
+//           include: [
+//               {           
+//                   model: await req.context.models.talent_batch,
+//                   as: "talent_batches",
+//                   include: [
+//                       {
+//                                   model: await req.context.models.batch,
+//                                   as: "taba_batch",
+//                                   include: [
+//                                         {
+//                                           model: await req.context.models.instructor,
+//                                           as: "batch_inst"
+                      
+//                                         }
+//                                       ]
+//                               }
+//                           ]
+//                 }
+//               ]
+//       })
+//       return res.status(200).json(result);
+//   } catch (error) {
+//       return res.status(404).send('no data found');
+//   }
+// }
+const findAll = async (req,res) =>{
   try {
-    const result = await req.context.models.talent.findAll();
-    return res.status(201).json({ result });
+      const result = await req.context.models.talent.findAll({
+          include: [
+              {           
+                  model: await req.context.models.talent_batch,
+                  as: "talent_batches",
+                  include: [
+                      {
+                                  model: await req.context.models.batch,
+                                  as: "taba_batch",
+                                  include: [
+                                        {
+                                          model: await req.context.models.instructor,
+                                          as: "batch_inst"
+                                        }
+                                      ]
+                              }
+                          ]
+                },
+                {
+                  model: req.context.models.talent_placement,
+                  as: "talent_placements",
+                  include : [
+                    {
+                      model: req.context.models.placement,
+                      as: "tapl_place",
+                    }
+                  ]
+                },
+              ]
+      })
+      return res.status(200).json(result);
   } catch (error) {
-    return res.status(404).send("no data found");
+      return res.status(404).send('no data found');
   }
-};
+}
+
+
+const findOne = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const result = await req.context.models.talent.findOne({
+      where: { curr_id: id },
+      include: [
+        {
+          model: req.context.models.talent_batch,
+          as: "curr_inst",
+        },
+        {
+          model: req.context.models.curriculum_materi,
+          as: "curriculum_materis",
+        },
+      ],
+    });
+
+    return res.json(result);
+  } catch (error) {
+    return next(error);
+  }
+}
+
+
 
 const createEmp = async (req, res) => {
   const { files, fields } = req.fileAttrb;
@@ -56,4 +140,5 @@ export default {
   findAll,
   createEmp,
   detail,
+  findOne
 };
