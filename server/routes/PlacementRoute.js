68bcf2913607ce2d4100d08756ecc329b530ff25
AJI SETIAJI <ajisetiaji101@gmail.com>
2022-05-16 11:27:47 +0700

    edit show image

--- server/routes/PlacementRoute.js
+++ server/routes/PlacementRoute.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import IndexController from "../controller/IndexController";
+import UploadDownloadHelper from "../helpers/UploadDownloadHelper";
 
 const router = Router();
 
 router.get("/", IndexController.PlacementController.List);
+router.get("/images/:filename", UploadDownloadHelper.showProductImage);
+router.delete("/:id", IndexController.PlacementController.hapusPlace);
 
 export default router;
