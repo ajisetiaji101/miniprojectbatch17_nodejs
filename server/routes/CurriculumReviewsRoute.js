ce115fbb255258a683bb1ea37a29784ed73668fd
afit08 <afitrabbani@gmail.com>
2022-05-19 16:53:59 +0700

    updated

--- server/routes/CurriculumReviewsRoute.js
+++ server/routes/CurriculumReviewsRoute.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import IndexController from "../controller/IndexController";
+import UploadDownloadHelper from "../helpers/UploadDownloadHelper";
 
 const router = Router()
 
 router.get('/',IndexController.CurriculumReviewsController.findAll)
 router.get('/:id',IndexController.CurriculumReviewsController.findOne)
+router.get("/images/:filename",UploadDownloadHelper.showProductImage)
 
 export default router
