d278e9bc83510e337d63c4a1c0fd33f658f8cb0a
venderdecken <ibnuashar9@gmail.com>
2022-05-23 21:43:28 +0700

    fixing update function

--- server/controller/JobController.js
+++ server/controller/JobController.js
@@ -78,31 +78,31 @@ const findOne = async (req, res) => {
 };
 
 const update = async (req, res) => {
-  const { fields, files } = req.fileAttrb;
+  const { files, fields } = req.fileAttrb;
   try {
     const jobs = await req.context.models.jobs.update(
       {
-        jobs_post_no: fields[0].value,
-        jobs_title: fields[1].value,
-        jobs_start_date: fields[2].value,
-        jobs_end_time: fields[3].value,
-        jobs_upto_salary: parseInt(fields[4].value),
-        job_upto_experience: fields[5].value,
-        jobs_description: fields[6].value,
-        jobs_primary_skill: fields[7].value,
-        jobs_secondary_skill: fields[8].value,
-        jobs_industry_type: fields[9].value,
-        jobs_working_type: fields[10].value,
-        jobs_publish: fields[11].value,
-        jobs_remotely: fields[12].value,
-        jobs_spec_education: fields[13].value,
-        jobs_benefit: fields[14].value,
-        jobs_specification: fields[15].value,
-        jobs_status: fields[16].value,
-        jobs_location: fields[17].value,
-        jobs_city: fields[18].value,
-        jobs_user_id: parseInt(fields[19].value),
-        jobs_client_id: parseInt(fields[20].value),
+        // jobs_post_no: fields[0].value,
+        jobs_title: fields[0].value,
+        jobs_start_date: fields[1].value,
+        jobs_end_time: fields[2].value,
+        jobs_upto_salary: parseInt(fields[3].value),
+        job_upto_experience: fields[4].value,
+        jobs_description: fields[5].value,
+        jobs_primary_skill: fields[6].value,
+        jobs_secondary_skill: fields[7].value,
+        jobs_industry_type: fields[8].value,
+        jobs_working_type: fields[9].value,
+        jobs_publish: fields[10].value,
+        jobs_remotely: fields[11].value,
+        jobs_spec_education: fields[12].value,
+        jobs_benefit: fields[13].value,
+        jobs_specification: fields[14].value,
+        jobs_status: fields[15].value,
+        jobs_location: fields[16].value,
+        jobs_city: fields[17].value,
+        jobs_user_id: parseInt(fields[18].value),
+        jobs_client_id: parseInt(fields[19].value),
         jobs_photo: files[0].newFilename,
         // jobs_photo: jobs_photo
         // jobs_id: fields[21].value,
